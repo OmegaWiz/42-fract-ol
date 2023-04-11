65855388d92018e3c40243cd53d5580ccc71f9c3
Kraiwish Kaiaywong <kkaiyawo@z2t17c3.42bangkok.com>
2023-04-11 08:58:10 +0700

    can now write a square

--- src/fractol.c
+++ src/fractol.c
@@ -6,11 +6,41 @@
 /*   By: kkaiyawo <kkaiyawo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 13:54:30 by kkaiyawo          #+#    #+#             */
-/*   Updated: 2023/04/11 08:54:01 by kkaiyawo         ###   ########.fr       */
+/*   Updated: 2023/04/11 08:57:55 by kkaiyawo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+void	write_square(t_data *img, int x, int y, int n)
+{
+	int	i;
+
+	i = 0;
+	while (i <= n)
+	{
+		my_mlx_pixel_put(img, x + i, y, 0x00FFFFFF);
+		i++;
+	}
+	i = 0;
+	while (i <= n)
+	{
+		my_mlx_pixel_put(img, x + i, y + n, 0x00FFFFFF);
+		i++;
+	}
+	i = 0;
+	while (i <= n)
+	{
+		my_mlx_pixel_put(img, x, y + i, 0x00FFFFFF);
+		i++;
+	}
+	i = 0;
+	while (i <= n)
+	{
+		my_mlx_pixel_put(img, x + n, y + i, 0x00FFFFFF);
+		i++;
+	}
+}
 
 int	main(int argc, char **argv)
 {
@@ -24,7 +54,8 @@ int	main(int argc, char **argv)
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "minilibx test");
 	img.img = mlx_new_image(mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bpp, &img.line_len, &img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	//my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	write_square(&img, 100, 100, 100);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
 	return (0);
